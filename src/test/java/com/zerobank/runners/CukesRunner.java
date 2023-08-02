7579599ce101f4def3317489894ba878a7ef900e
eren.cengel <eren.cengel@scalefocus.com>
2023-08-02 20:01:21 +0300

    project setup

---
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -0,0 +1,16 @@
+package com.zerobank.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/zerobank/step_defs",
+        plugin = {"json:target/cucumber.json"},
+        dryRun = false,
+        tags = ""
+)
+public class CukesRunner {
+}
